ca7d7d7709ee9e09bd4ba52244341b575e43dc40
Giovanni Ciotta <giociotta@gmail.com>
2022-05-09 18:00:18 +0200

    es dadi completo

--- dadi/script-dadi.js
+++ dadi/script-dadi.js
@@ -1,4 +1,5 @@
 const btnPlayer = document.getElementById("dado");
+let risFinale = document.querySelector (`.risultato`);
 
 btnPlayer.addEventListener(`click`,
 function() {
@@ -6,6 +7,25 @@ function() {
     console.log(dadoPlayer);
     const dadoPc = Math.floor(Math.random() * 6) +1;
     console.log(dadoPc);
+
+    if (dadoPlayer > dadoPc ){
+        console.log("win");
+        risFinale.innerHTML = "Hai Vinto"
+
+    } else if (dadoPlayer === dadoPc ){
+        console.log("draw");
+        risFinale.innerHTML = "Hai Pareggiato"
+
+    }
+    else {
+        console.log("lose");
+        risFinale.innerHTML = "Hai Perso"
+    }
+        
+
+    
+
+
 }
 
 )
