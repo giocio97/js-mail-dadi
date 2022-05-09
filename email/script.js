bdb926b80e36c18724f5ae4d6de0967e5d7e34c5
Giovanni Ciotta <giociotta@gmail.com>
2022-05-09 15:52:43 +0200

    inizio es email

---
+++ email/script.js
@@ -0,0 +1,14 @@
+// lista email di chi puo accedere
+const emailPass =["giociotta@gmail.com", "ciao@hello.it", "liberato@libero.it", "vivagesu@hotmail.it"]
+
+
+// Chiedo all’utente la sua email
+let emailUser
+
+
+
+// controllo che sia nella lista di chi può accedere
+
+
+
+// stampa un messaggio  sull’esito del controllo.
