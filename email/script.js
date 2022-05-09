19420e33e14b05831088805d179c37ec23b18dec
Giovanni Ciotta <giociotta@gmail.com>
2022-05-09 16:11:38 +0200

    avanzamento es email

--- email/script.js
+++ email/script.js
@@ -1,9 +1,23 @@
+const inputEmail = document.querySelector("input");
+console.log(inputEmail);
+const myButton = document.querySelector("button");
+console.log(myButton); 
+const myOutput = document.querySelector(".access"); 
+
+
 // lista email di chi puo accedere
-const emailPass =["giociotta@gmail.com", "ciao@hello.it", "liberato@libero.it", "vivagesu@hotmail.it"]
+const emailPass =["giociotta@gmail.com", "ciao@hello.it", "liberato@libero.it", "vivagesu@hotmail.it"];
 
 
 // Chiedo all’utente la sua email
-let emailUser
+let emailUser = inputEmail.value;
+
+myButton.addEventListener("click",
+    function(){
+        myOutput.append(emailPass [emailUser]);
+    }
+
+)
 
 
 
